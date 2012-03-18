1fa5aceb4de980b708406c8de01004dc1922efad
Andreas Richter <ar@anrichter.net>
2012-03-18 17:52:21 +0100

    Setup creates symlinks on non mingw32 systems.

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 for file in *; do
-  if [ "$file" != "install.sh" ]; then
-    destination="$HOME/.$file"
-    if [ -e "$destination" ]; then
-      echo "Dotfile $destination already exists. Leave it untouched"
-    else
+  destination="$HOME/.$file"
+  if [ -e "$destination" ]; then
+    echo "Dotfile $destination already exists. Leave it untouched"
+  else
+    if [ $file != 'install.sh' ]; then
       echo "Create dotfile $destination"
-      cp "$PWD/$file" "$destination"
+      if [ "$MSYSTEM" = "MINGW32" ]; then
+        cp "$PWD/$file" "$destination"
+      else
+        ln -s "$PWD/$file" "$destination"
+      fi
     fi
   fi
 done
