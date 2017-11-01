2cc0779503e107fce2c606f66af42a083ab1e699
Andreas Richter <ar@anrichter.net>
2017-11-01 10:07:41 +0100

    Removed path for dnx, GitTfs and atom
    * GitTfs is installed through Chocolatey with an own path entry
    * atom is no longer used
    * dnx is obsolete

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -81,15 +81,11 @@ $currentPSModulePath = [Environment]::GetEnvironmentVariable("PSModulePath", "Ma
 # Path
 #
 
-prepend-path "${env:UserProfile}\.dnx\bin"
-
 append-path "${env:ProgramFiles(x86)}\vim\vim74"
 append-path "${env:ProgramFiles}\Git\cmd"
 append-path "${env:ProgramFiles}\Git\usr\bin"
 append-path "${env:ProgramFiles}\Microsoft VS Code\bin"
 append-path "${env:UserProfile}\bin"
-append-path "${env:UserProfile}\bin\GitTfs"
-append-path "${env:LocalAppData}\atom\bin"
 
 #
 # Posh-Git
